5b0aa9f6cc3decbc2fac7fdb787cbd11dac727fd
ConacBreslin <conacbreslin@sky.com>
2021-10-31 18:16:19 +0000

    Customize admin

--- gins/admin.py
+++ gins/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Gin, Distillery
 
+class GinAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'distillery',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('price',)
+   
 # Register models.
-admin.site.register(Gin)
+admin.site.register(Gin, GinAdmin)
 admin.site.register(Distillery)
 
